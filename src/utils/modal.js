caf558e539769b4ccadd74b245d2a4c0b68444af
kyeongbeom kim <xops09685@gmail.com>
2024-09-01 17:08:44 +0900

    updated : first commit

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -1,10 +1,10 @@
 export const modalSetting = {
 	SampleModal: () => {
 		console.log('샘플 모달');
-		return import('@/components/modal/SampleModal1/SampleModal1.jsx');
+		return import('@/components/modal/SampleModal1/SampleModal1');
 	},
 	SampleModal2: () => {
 		console.log('샘플 모달2');
-		return import('@/components/modal/SampleModal2/SampleModal2.jsx');
+		return import('@/components/modal/SampleModal2/SampleModal2');
 	},
 };
