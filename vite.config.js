d6be0fe525cd0b5af966156317b8926fde86fe03
kimkyeongbeom <xops09685@gmail.com>
2025-03-19 12:30:37 +0900

    update: vite proxy 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
-import path from "path"; // Node.js의 path 모듈을 가져옴
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
   server: {
-    https: false,
+    https: true,
     // host: '127.0.0.1',
     // port: '5173',
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true, // 요청 출처를 백엔드 도메인으로 변경
+        secure: false, // HTTPS 검증 비활성화 (로컬 백엔드가 HTTP인 경우)
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // @를 /src 경로로 설정
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   esbuild: {
