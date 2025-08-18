74fa5a1d17674dee0fb98bb5fdeb7b58c1e5c15c
kimkyeongbeom <xops09685@gmail.com>
2025-08-18 11:16:08 +0900

    update: react-query추가 및 module부분 팩토리 패턴 적용

--- src/index.jsx
+++ src/index.jsx
@@ -7,15 +7,31 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./pages";
 import "@/assets/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <Suspense>
-        <RouterProvider router={router} />
-      </Suspense>
-      <ModalFrame />
-      <Spinner />
+      <QueryClientProvider client={queryClient}>
+        <Suspense>
+          <RouterProvider router={router} />
+        </Suspense>
+        <ModalFrame />
+        <Spinner />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+        />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
